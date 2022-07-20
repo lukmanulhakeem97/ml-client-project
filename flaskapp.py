c6539ad457fda05347a4e50fb0c193f1872355c5
lukmanulhakeem97 <69229526+lukmanulhakeem97@users.noreply.github.com>
2022-07-20 18:21:52 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 19 14:39:05 2022
-
-@author: Datamites
-"""
-
 from flask  import Flask, render_template, request
 from statsmodels.tsa.ar_model import AutoRegResults
 import numpy as np
@@ -33,17 +26,11 @@ def predict():
             prediction = list(map(round, list_))
         #if date!=None:
             #pred_loaded_model = model.predict(start=len(engineoil_df_train), end=len(engineoil_df_train)+9, dynamic=False)
-
-        #output=round(prediction[0],2)
-        #if output<0:
-        #    return render_template('index.html',pred=0)
-        #else:
-            #pred = "You Can Sell The Car at {}".format(output)
-            #return render_template('index.html',pred=pred)
+            
         return render_template('index.html',pred=prediction)
     else:
         return render_template('index.html')
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port='8000', debug=True)
+    app.run(host='0.0.0.0', port='8000')
 
